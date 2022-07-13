e8f241831e31dabb6aa5d79b69e3636aab4d29f0
Sergio Hernandez Reyes <sergio.hdzreyes@gmail.com>
2022-07-13 00:00:34 -0500

    Documentation added

--- src/servers_management.h
+++ src/servers_management.h
@@ -14,12 +14,32 @@
 
 #define PROGRAM_NAME "ssf-servers"
 
+/**
+ * Print instructions for command usage.
+ */
 void print_help();
 
+/**
+ * Process parameters passed to program.
+ * @param argc Parameters quantity
+ * @param argv Parameters array
+ */
 void parse_options(int argc, char *argv[]);
 
+/**
+ * Print a servers list to screen
+ * @param cfg Structure that contain config file info
+ * @param config_file_path Full path of configuration file
+ */
 void show_servers_list(config_t *cfg, char *config_file_path);
 
+/**
+ * Initialize all basic information.
+ * @param cfg Structure that contain config file info
+ * @param config_file Name of configuration file
+ * @param config_file_path Full path of configuration file
+ * @return
+ */
 int configuration_init(config_t *cfg, const char *config_file, char **config_file_path);
 
 /**
