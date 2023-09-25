3e2f2afc0f223b702730b52dc0072b9e3d31c24c
Sergio Hernandez Reyes <sergio.hdzreyes@gmail.com>
2023-09-24 23:15:37 -0600

    Git project validation moved to main function.

--- src/ssf.c
+++ src/ssf.c
@@ -2,6 +2,14 @@
 #include "ssf_app.h"
 
 int main( int argc, char *argv[] ) {
+    DIR *dir = opendir(".git");
+
+    if (!dir) {
+        printf("No se encontro un proyecto Git en el directorio actual\n");
+        return 1;
+    }
+    closedir(dir);
+
     g_resources_register(ssf_get_resource());
 
     return g_application_run(G_APPLICATION(ssf_app_new()), argc, argv);
