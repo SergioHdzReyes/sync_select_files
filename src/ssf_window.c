81d4878b3a73f718bbe002776334ecde5f363a44
Sergio Hernandez Reyes <sergio.hdzreyes@gmail.com>
2023-09-30 23:19:48 -0600

    List of affected files showed for selection of the user.

--- src/ssf_window.c
+++ src/ssf_window.c
@@ -11,7 +11,13 @@ struct _SsfAppWindow {
     GtkApplicationWindow parent;
     GApplication *app;
 
-    GtkWidget *SsfDiffGridMain;
+    GtkWidget *SsfGridDiffTools;
+    GtkWidget *SsfGridDiffDifferences;
+
+    /* Necessary for DiffTools */
+    GtkWidget *SsfScrolledWindowFiles;
+    GtkWidget *SsfGridAffectedFiles;
+
     GtkWidget *SsfListFiles;
     //GtkWidget *SsfFrameSourceCode;
     GtkWidget *SsfLocalFileDiff;
@@ -21,7 +27,7 @@ struct _SsfAppWindow {
     SsfSelectServerDialog *selectServerDialog;
     SsfInvalidProjectDialog *invalidProjectDialog;
 
-} AppWindow;
+} AppWindow = {NULL};
 
 G_DEFINE_TYPE(SsfAppWindow, ssf_app_window, GTK_TYPE_APPLICATION_WINDOW);
 
@@ -29,78 +35,121 @@ void ssf_app_window_update_status (SsfAppWindow *pWindow);
 
 static void ssf_app_window_init (SsfAppWindow *pWindow) {
     gtk_widget_init_template(GTK_WIDGET(pWindow));
-
-    pWindow->selectServerDialog = NULL;
-    pWindow->invalidProjectDialog = NULL;
 }
 
 static void ssf_app_window_class_init (SsfAppWindowClass *pClass) {
     gtk_widget_class_set_template_from_resource(GTK_WIDGET_CLASS(pClass), "/sync_select_files/ssf/ssf.AppWindow.glade");
 
-    gtk_widget_class_bind_template_child(GTK_WIDGET_CLASS(pClass), SsfAppWindow, SsfDiffGridMain);
     gtk_widget_class_bind_template_child(GTK_WIDGET_CLASS(pClass), SsfAppWindow, SsfContainerBox);
+
+    gtk_widget_class_bind_template_child(GTK_WIDGET_CLASS(pClass), SsfAppWindow, SsfGridDiffTools);
+    gtk_widget_class_bind_template_child(GTK_WIDGET_CLASS(pClass), SsfAppWindow, SsfGridAffectedFiles);
+
+    gtk_widget_class_bind_template_child(GTK_WIDGET_CLASS(pClass), SsfAppWindow, SsfGridDiffDifferences);
 }
 
 SsfAppWindow * ssf_app_window_new (GApplication *pApp) {
     AppWindow.app = pApp;
     SsfAppWindow * pWindow = g_object_new (SSF_APP_WINDOW_TYPE, "application", pApp, NULL);
 
-    //process_differences(pWindow);
+    ssf_process_differences(pWindow);
 
     return pWindow;
 }
 
 void ssf_process_differences(SsfAppWindow *pWindow) {
-    GtkSourceBuffer *buffer = gtk_source_buffer_new(NULL);
+    if (!ssf_diff_start()) {
+        return;
+    }
 
-    ssf_diff_start();
-
-    //ME QUEDE AQUI
-    gtk_text_buffer_set_text(&buffer->parent_instance, "Codigo de prueba", -1);
-
-    pWindow->SsfLocalFileDiff = gtk_source_view_new_with_buffer(buffer);
-
-    gtk_source_view_set_show_line_numbers((GtkSourceView *)pWindow->SsfLocalFileDiff, TRUE);
-    gtk_container_add((GtkContainer *) pWindow->SsfContainerBox, pWindow->SsfLocalFileDiff);
-
-    // Comienza mostrar archivos
-    enum {
-        COLUMN_INT,
-        COLUMN_STRING,
-        COLUMN_INT2,
-        N_COLUMNS
-    };
-    GtkListStore *list_store = gtk_list_store_new(N_COLUMNS, G_TYPE_INT, G_TYPE_STRING, G_TYPE_INT);
+    GtkListStore *list_store;
     GtkTreeIter iter;
-    gtk_list_store_append(list_store, &iter);
-    gtk_list_store_set(list_store, &iter,
-                       COLUMN_INT, 1,
-                       COLUMN_STRING, "SERGIO",
-                       COLUMN_INT2, 1234,
-                       -1);
+    gint i;
+    //GtkWidget *checkbox[diff_count];
+
+    list_store = gtk_list_store_new (5,
+                                     G_TYPE_INT,
+                                     G_TYPE_STRING,
+                                     G_TYPE_STRING,
+                                     G_TYPE_STRING,
+                                     G_TYPE_STRING);
+
+    for (i = 0; i < diff_count; i++)
+    {
+        // Add a new row to the model
+        gtk_list_store_append (list_store, &iter);
+        //checkbox[i] = gtk_check_button_new_with_label("select");
+
+        char *transaction;
+        if (strcmp(operation[i], "del.") == 0) {
+            transaction = "Eliminar";
+        } else if (strcmp(operation[i], "send") == 0){
+            transaction = "Enviar";
+        } else {
+            transaction = "Desconocido";
+        }
+
+        gtk_list_store_set (list_store, &iter,
+                            0, i+1,
+                            1, filepath[i],
+                            2, transaction,
+                            3, "Ver",
+                            //4, checkbox[i],
+                            4, "Seleccionar",
+                            -1);
+    }
 
     GtkWidget *tree_view = gtk_tree_view_new_with_model((GtkTreeModel *) list_store);
     GtkCellRenderer *renderer = gtk_cell_renderer_text_new();
+    GtkTreeViewColumn *number, *pathfile, *action, *see, *select;
 
-    GtkTreeViewColumn *number, *filename;
-    number = gtk_tree_view_column_new_with_attributes("Numero", renderer, "text", 0, NULL);
-    filename = gtk_tree_view_column_new_with_attributes("Nombre de archivo", renderer, NULL);
+    // First column
+    number = gtk_tree_view_column_new_with_attributes("#", renderer, "text", 0, NULL);
     gtk_tree_view_append_column((GtkTreeView *) tree_view, number);
-    gtk_tree_view_append_column((GtkTreeView *) tree_view, filename);
 
-    gtk_grid_attach(GTK_GRID(pWindow->SsfDiffGridMain), tree_view, 1, 1, 1, 1);
-    // Termina mostrar archivos
+    // Second column
+    pathfile = gtk_tree_view_column_new_with_attributes("Ruta de archivo", renderer, "text", 1, NULL);
+    gtk_tree_view_append_column((GtkTreeView *) tree_view, pathfile);
 
-    gtk_widget_show_all(pWindow->SsfDiffGridMain);
-    gtk_widget_show_all(pWindow->SsfLocalFileDiff);
+    // Third column
+    action = gtk_tree_view_column_new_with_attributes("Accion", renderer, "text", 2, NULL);
+    gtk_tree_view_append_column((GtkTreeView *) tree_view, action);
+
+    // Fourth column
+    see = gtk_tree_view_column_new_with_attributes("Ver", renderer, "text", 3, NULL);
+    gtk_tree_view_append_column((GtkTreeView *) tree_view, see);
+
+    // Fifth column
+    //GtkCellRenderer *toggle = gtk_cell_renderer_toggle_new();
+    //select = gtk_tree_view_column_new_with_attributes("Seleccionar", toggle, 4, NULL);
+    select = gtk_tree_view_column_new_with_attributes("Seleccionar", renderer, "text", 4, NULL);
+    gtk_tree_view_append_column((GtkTreeView *) tree_view, select);
+
+    gtk_grid_attach(GTK_GRID(pWindow->SsfGridAffectedFiles), tree_view, 0, 0, 1, 1);
+    /* END DiffTools */
+
+//    GtkSourceBuffer *buffer = gtk_source_buffer_new(NULL);
+//
+//    ssf_diff_start();
+//
+//    //ME QUEDE AQUI
+//    gtk_text_buffer_set_text(&buffer->parent_instance, "Codigo de prueba", -1);
+//
+//    pWindow->SsfLocalFileDiff = gtk_source_view_new_with_buffer(buffer);
+//
+//    gtk_source_view_set_show_line_numbers((GtkSourceView *)pWindow->SsfLocalFileDiff, TRUE);
+//    gtk_container_add((GtkContainer *) pWindow->SsfContainerBox, pWindow->SsfLocalFileDiff);
+//
+//    gtk_widget_show_all(pWindow->SsfGridDiffDifferences);
+//    gtk_widget_show_all(pWindow->SsfLocalFileDiff);
+    gtk_widget_show_all(pWindow->SsfGridAffectedFiles);
 }
 
 /**
 * SIGNALS
 */
 
-void ssf_check_version_control(GtkWidget *widget, gpointer user_data)
-{
+void ssf_check_version_control(GtkWidget *widget, gpointer user_data) {
     DIR *dir = opendir(".git");
 
     if (dir) {
@@ -121,4 +170,9 @@ void ssf_select_server_submenu(GtkMenuItem *menuitem, gpointer user_data) {
 
     pWindow->selectServerDialog = ssf_select_server_dialog_new(pWindow, TRUE, user_data);
     gtk_window_present(GTK_WINDOW(pWindow->selectServerDialog));
+}
+
+void ssf_app_submenu_exit(GtkMenuItem *menuitem, gpointer user_data)
+{
+    g_application_quit(AppWindow.app);
 }
