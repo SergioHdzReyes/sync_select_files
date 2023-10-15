70f7ae7f2f23d3c0b80c027dec18ef5413dc9807
Sergio Hernandez Reyes <sergio.hdzreyes@gmail.com>
2023-10-15 15:27:25 -0600

    Added validations for compile.

--- reinstall.sh
+++ reinstall.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 # Script to rebuild and reinstall program
 
+validate_status_command() {
+  if [[ $? -ne 0 ]]
+  then
+    echo "SSF - An error has occurred on $1!"
+    exit
+  fi
+}
+
 ./bootstrap
+
+validate_status_command "bootstrap"
+
 ./configure
 
+validate_status_command "configure"
+
 make
+
+validate_status_command "make"
+
 sudo make install
+validate_status_command "make install"
